ee6875900fb9933c3aaddd9b2a20f8c42e47d5a4
황성욱 <dnrdl1109@gmail.com>
2026-03-17 18:56:44 +0900

    흑백모드 추가

--- videoRecorder.py
+++ videoRecorder.py
@@ -10,12 +10,19 @@ if video.isOpened():
     
     is_record = False
     is_flip = False
+    is_gray = False
     
     while True:
         valid, img = video.read()
         if not valid:
             break
         height, width = img.shape[:2]
+        
+        if is_gray:
+            # 컬러를 흑백으로 변환 
+            img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            # VideoWriter와 호환성을 위해 다시 BGR로 변환
+            img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
         
         if is_flip:
             img = cv.flip(img, 1) # 좌우반전
@@ -36,6 +43,9 @@ if video.isOpened():
                 writer = cv.VideoWriter('output.avi', cv.VideoWriter_fourcc(*target_fourcc), fps, (width, height))
         elif key == ord('f'): # f: Flip 모드 변환
             is_flip = not is_flip
+        elif key == ord('g'): # g: 흑백 모드 변환
+            is_gray = not is_gray
+            
     video.release()
     writer.release()
 
